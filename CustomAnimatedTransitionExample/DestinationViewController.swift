8d3079ece32e237fbc950f8c9f8d7935cc80a30d
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-07-08 23:25:11 +0900

    cteate DestinationViewController

---
+++ CustomAnimatedTransitionExample/DestinationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DestinationViewController.swift
+//  CustomAnimatedTransitionExample
+//
+//  Created by KojimaTatsuya on 2017/07/08.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class DestinationViewController: UIViewController {
+    
+}
