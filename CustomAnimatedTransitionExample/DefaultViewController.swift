d6eb61f3ce2de50a58b9ef3739275d952d7c0ca1
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-07-09 01:29:07 +0900

    create DefaultViewController

---
+++ CustomAnimatedTransitionExample/DefaultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultViewController.swift
+//  CustomAnimatedTransitionExample
+//
+//  Created by KojimaTatsuya on 2017/07/09.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class DefaultViewController: UIViewController {
+    
+}
