ff95de483f8ade76d9de1976beb889e83d9af515
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-07-09 01:22:24 +0900

    create NavigationController

---
+++ CustomAnimatedTransitionExample/NavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  NavigationController.swift
+//  CustomAnimatedTransitionExample
+//
+//  Created by KojimaTatsuya on 2017/07/09.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        
+        delegate = self
+    }
+    
+}
+
+extension NavigationController: UINavigationControllerDelegate {
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomAnimatedTransitioning(operation: operation)
+    }
+    
+}
